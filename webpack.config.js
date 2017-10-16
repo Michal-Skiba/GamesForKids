66e0f570c532a2cdfa6b817eafe16d349424fa00
Michał Skiba <skiba_michal@wp.pl>
2017-10-16 20:27:57 +0200

    First step of layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,51 @@
-const path = require("path");
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: "./js/zadanie04.jsx",
-    output: {
-        path: path.resolve("js"),
-        filename: "out.js"
+    entry : {
+        'js/out.js': './js/app.jsx',
+        'css/style.css~': './css/main.scss'
+    },
+    output : {
+        path: __dirname+'/',
+        filename: '[name]'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './',
+        port: 3001
     },
     watch: true,
     module: {
-        loaders: [{
-            test: /\.jsx$/, exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015','stage-2', 'react']
+        loaders: [
+            {
+                test: /\.jsx$/, exclude: /node_modules/,
+                loader:	'babel-loader',
+                query: { presets: ['es2015','stage-2', 'react'] }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
             }
-        }]
-    }
-}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('./css/style.css')
+
+    ]
+};
